2fe7c42afc4015a0d12013136b76cf058b6ce8a5
dutreras <dutreras369@gmail.com>
2022-06-23 01:44:35 -0400

    add h2 database test

--- webservices/src/main/java/com/adareloise/restfull/model/User.java
+++ webservices/src/main/java/com/adareloise/restfull/model/User.java
@@ -2,6 +2,10 @@ package com.adareloise.restfull.model;
 
 import java.util.Date;
 
+import javax.annotation.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
@@ -14,9 +18,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
 public class User {
 	
-	
+	@Id
+	@GeneratedValue
 	private Integer id;
 	@Size(min=2, message = "Name should have atleast 2 characters")
 	private String name;
